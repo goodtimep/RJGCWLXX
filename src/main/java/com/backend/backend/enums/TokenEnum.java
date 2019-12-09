5bbe4a9c057490f61c4cdadcd4f3498d942c154e
goodtimep <879636706@qq.com>
2019-12-09 15:59:29 +0800

    init 登录
    注册

--- src/main/java/com/backend/backend/enums/TokenEnum.java
+++ src/main/java/com/backend/backend/enums/TokenEnum.java
@@ -9,14 +9,14 @@ import lombok.Getter;
  */
 @Getter
 public enum TokenEnum {
-    ACCESS_TOKEN_EXPIRE_TIME("token过期时间(s)", "30"),
+    ACCESS_TOKEN_EXPIRE_TIME("token过期时间(s)", "3000"),
     PAYLOAD_USER_TAG("token有效载荷中用户Id标识", "userId"),
     PAYLOAD_ROLE_TAG("token有效载荷中角色Id标识（逗号隔开）", "roleIds"),
     PAYLOAD_CREATE_TIME_TAG("token有效载荷中创建时间标识", "createTime"),
     REFRESH_TOKEN_EXPIRE_TIME("refreshToken有效载荷中创建时间标识(s)（7天）", "604800"),
     ENCRYPT_JWT_KEY("accessToken JWT认证加密私钥(Base64加密)后的值", "bXklMjBuYW1lJTIwaXMlMjBnb29kdGltcA=="),
     ENCRYPT_REFRESH_JWT_KEY("refreshToken认证加密私钥(Base64加密)后的值", "JXU3MjMxJXU0RjYwJXU1NTRBJXU1OTBGJXU2OEE2JXU2NUNC"),
-    SHIRO_CACHE_EXPIRE_TIME("Shiro缓存过期时间-5分钟-5*60(秒为单位)(一般设置与AccessToken过期时间一致)", "30");
+    SHIRO_CACHE_EXPIRE_TIME("Shiro缓存过期时间-5分钟-5*60(秒为单位)(一般设置与AccessToken过期时间一致)", "3000");
     private String name;
     private String code;
 
